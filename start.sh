8f342d051c171818eae9c28d43b5dd4df087163a
Sven Wagener <sven@awesome.ug>
2017-07-28 14:04:27 +0200

    Inbetween

--- start.sh
+++ start.sh
@@ -2,9 +2,19 @@
 
 dirname=${PWD##*/}
 
-docker-compose up -d
-
-if [ ! -d ./src ]; then
-    git submodule add git@github.com:awsmug/alexa-sdk.git ./src
+if [ ! -d ./develop ]; then
+    git submodule add git@github.com:api-api/structure-billomat.git ./develop
     git submodule init
 fi
+
+git submodule update
+
+if [ ! -d ./vendor ]; then
+    composer install
+fi
+
+if [ ! -d ./node_modules ]; then
+    npm install
+fi
+
+docker-compose up -d
